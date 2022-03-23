fc9ac1d310722128b1cce9664e21cafc94504690
Maxwell Veer <maxaveer@gmail.com>
2022-03-23 19:09:04 +0000

    added edit buttons to /urls and they redirect to a page where you can change the longurl associated with the selected shorturl

--- express_server.js
+++ express_server.js
@@ -18,9 +18,10 @@ app.get("/", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL
   const longURL = urlDatabase[shortURL]
+  const templateVars = {shortURL: shortURL}
   console.log(longURL)
   //console.log(req.params, urlDatabase)
-  res.redirect(longURL);
+  res.redirect(templateVars);
 });
 
 app.get('/urls', (req, res) => {
@@ -28,17 +29,24 @@ app.get('/urls', (req, res) => {
   res.render("urls_index", templateVars);
 });
 app.post("/urls/:shortURL/delete", (req, res) => {
-  console.log(req.params)
+  //console.log(req.params)
   delete urlDatabase[req.params.shortURL]
-  res.redirect("/urls")
-})
+  res.redirect("/urls/")
+});
+app.post("/urls/:shortURL", (req, res) => {
+  console.log(req.body, req.params)
+  urlDatabase[req.params.shortURL] = `http://${req.body.longURL}`;
+  res.redirect(`/urls`)
+});
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
   let randoString = generateRandomString();
   urlDatabase[randoString] = `http://${req.body.longURL}`;
-  console.log(urlDatabase)
   res.redirect(`/urls/${randoString}`)
 });
+
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = `http://${req.body.longURL}`
+})
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
